4442c86936be837727edccbbc6fe2c56f74ebfe6
Amarnath <sharewithamar@gmail.com>
2019-11-11 16:53:11 -0800

    class component basics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/*
+ function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +22,44 @@ function App() {
       </header>
     </div>
   );
+} */
+class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      monsters: [
+        {
+          name :'Frankenstein'
+        },
+        {
+          name : 'Draculla'
+        },
+        {
+          name : 'Zombie'
+        },
+      ]
+   }
+  }
+  render(){
+    return (
+    <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+       Hello {this.state.name} component Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React 
+      </a>
+      <button onClick={()=> this.setState({name:' Anira'}) } >Change Text</button>
+    </header>
+  </div>
+  )
+  }
 }
-
 export default App;
