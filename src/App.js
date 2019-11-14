53566e15dac03af8da5bf358735bf8b120f477c2
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2019-11-14 16:20:54 +0530

    search bar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField:''
     };
   }
   componentDidMount() {
@@ -17,6 +18,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <input type="search" placeholder="search monsters" 
+      onChange={e =>this.setState({searchField:e.target.value })}/>
         <CardList monsters={this.state.monsters} />
       </div>
     );
