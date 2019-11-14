e55a7dd503d63e1131e7236378f51e1b0a6b7af3
Amarnath <sharewithamar@gmail.com>
2019-11-13 17:35:11 -0800

    card component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, {Component} from 'react';
-import './App.css';
-import {CardList} from './components/card-list/card-list.component'
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state ={
+    this.state = {
       monsters: []
-   }
+    };
   }
-  componentDidMount()
-  {
-    fetch('https://jsonplaceholder.typicode.com/users').then( response => response.json()).then(data => this.setState({monsters: data}))
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => this.setState({ monsters: data }));
   }
 
-  render(){
+  render() {
     return (
       <div className="App">
-      <CardList name ='Amar'>      
-      {
-        this.state.monsters.map( monster => <h1 key={monster.id}>{monster.name}</h1>)
-      }
-      </CardList>
-  </div>
-  )
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
   }
 }
 export default App;
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import './card-list.styles.css';
-export const CardList = (props) => {
-    console.log(props)
-    return (
-        <div className="card-list">
-        {props.children}
-        </div>
-    )
-}
+import React from "react";
+import "./card-list.styles.css";
+import { Card } from "../card/card.component";
+export const CardList = props => {
+  return (
+    <div className="card-list">
+      {props.monsters.map(monster => (
+        // <h1 key={monster.id}>{monster.name}</h1>
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
+};
