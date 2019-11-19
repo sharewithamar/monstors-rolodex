4817ee24674138cd75dae57bde5129217edbe93d
Amarnath <sharewithamar@gmail.com>
2019-11-19 06:10:21 -0800

    bind methods

--- src/App.js
+++ src/App.js
@@ -9,23 +9,27 @@ class App extends Component {
       monsters: [],
       searchField: ""
     };
+    this.handleChange = this.handleChange.bind(this);
   }
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
       .then(data => this.setState({ monsters: data }));
   }
-
+  handleChange(e) {
+    this.setState({ searchField: e.target.value });
+  }
   render() {
     const { monsters, searchField } = this.state;
     const filterdMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
+
     return (
       <div className="App">
         <SearchBox
           placeholder="search monsters"
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         >
           {" "}
         </SearchBox>
