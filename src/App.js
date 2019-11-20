ca6839eb823023967ea21c37cd5b331d8b978313
Amarnath <sharewithamar@gmail.com>
2019-11-19 17:53:45 -0800

    background css

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ class App extends Component {
       monsters: [],
       searchField: ""
     };
-    this.handleChange = this.handleChange.bind(this);
+    // this.handleChange = this.handleChange.bind(this);  // use arrow function on methods to avoid manual binding
   }
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
       .then(data => this.setState({ monsters: data }));
   }
-  handleChange(e) {
+  handleChange = e => {
     this.setState({ searchField: e.target.value });
-  }
+  };
   render() {
     const { monsters, searchField } = this.state;
     const filterdMonsters = monsters.filter(monster =>
@@ -27,6 +27,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1> Mosters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
