c5945a5469775f465e15f56aafd2964ce324db14
Amarnath <sharewithamar@gmail.com>
2019-11-20 17:03:26 -0800

    Updates

---
+++ precache-manifest.eff40e8210c6a0086aadffff3e51ca7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d6687b548f1e5d5122c61daf65e6cd1",
+    "url": "/monstors-rolodex/index.html"
+  },
+  {
+    "revision": "b8dd588ed9e2f104d3d9",
+    "url": "/monstors-rolodex/static/css/main.799c7e19.chunk.css"
+  },
+  {
+    "revision": "804ae4df6e33bcb559ff",
+    "url": "/monstors-rolodex/static/js/2.66db94e0.chunk.js"
+  },
+  {
+    "revision": "b8dd588ed9e2f104d3d9",
+    "url": "/monstors-rolodex/static/js/main.74dab1f4.chunk.js"
+  },
+  {
+    "revision": "f2e58df62084dcd05758",
+    "url": "/monstors-rolodex/static/js/runtime-main.a56b8e03.js"
+  }
+]);
